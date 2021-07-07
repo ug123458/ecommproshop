089d1dfad28c02a011a01a0dea09ad776d02f313
Utkarsh <ug123458@gmail.com>
2021-07-07 18:44:30 +0530

    Navbar and React bootstrap Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
-
-const App=()=> {
+import React from "react"
+import {Container} from 'react-bootstrap'
+import Head from "./components/Head"
+import Foot from "./components/Foot"
+const App = () => {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Head />
+      <main>
+        <Container className='py-3'>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Foot />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Foot.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Container,Row,Col } from "react-bootstrap"
+const Foot = () => {
+  return <footer>
+    <Container>
+      <Row>
+        <Col className='text-center py-3'>
+          Copyright &copy; Proshop
+        </Col>
+      </Row>
+    </Container>
+    </footer>
+}
+
+export default Foot
