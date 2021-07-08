e457c05493d8e54b399b032257a952e62bdf2104
Utkarsh <ug123458@gmail.com>
2021-07-08 17:25:56 +0530

    Builded Product Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
-import React from "react"
-import { Container } from "react-bootstrap"
-import Head from "./components/Head"
-import Foot from "./components/Foot"
-import HomeScreen from "./Screens/HomeScreen"
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Head from './components/Head'
+import Foot from './components/Foot'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
 const App = () => {
   return (
-    <>
+    <Router>
       <Head />
       <main>
         <Container className='py-3'>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Foot />
-    </>
+    </Router>
   )
 }
 
