5d6c8ac27b162b83a48f3b87ecc440d0053028bf
Utkarsh <ug123458@gmail.com>
2021-07-08 17:43:25 +0530

    Basic nodejs setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const products = require('../backend/Data/products')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API is Running')
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
+})
+
+app.listen(5000, console.log('server started on port 5000'))
