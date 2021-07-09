638610c27c1a5e8c4dd653303f115600c7715335
Utkarsh <ug123458@gmail.com>
2021-07-09 16:14:00 +0530

    nodemon concurrently setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const products = require('../backend/Data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from '../backend/Data/products.js'
 const app = express()
+dotenv.config()
 
 app.get('/', (req, res) => {
   res.send('API is Running')
@@ -15,4 +17,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('server started on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(`server running on ${process.env.NODE_ENV} on port ${PORT}`)
+)
