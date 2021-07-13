3a6a974eab5401a1428072c3cef8729e25b45f23
Utkarsh <ug123458@gmail.com>
2021-07-13 16:54:36 +0530

    added user reducer and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,26 @@ import {
   productListReducer,
 } from './reducers/productReducer'
 import { cartReducer } from './reducers/cartReducer'
+import { userLoginReducer } from './reducers/userReducer'
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 })
 
 const cartstorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
+const userInfostorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
 const intialState = {
   cart: { cartItems: cartstorage },
+  userLogin: { userInfo: userInfostorage },
 }
 const middleware = [thunk]
 const store = createStore(
