0cbe78250e8df877798f9a137ec5b7e03ee12f80
Utkarsh <ug123458@gmail.com>
2021-07-13 19:59:59 +0530

    added register user screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
 import CartScreen from './Screens/CartScreen'
 import LoginScreen from './Screens/LoginScreen'
+import RegisterScreen from './Screens/RegisterScreen'
 const App = () => {
   return (
     <Router>
       <Head />
       <main>
         <Container className='py-3'>
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
--- frontend/src/Screens/RegisterScreen.js
+++ frontend/src/Screens/RegisterScreen.js
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from './../components/Message'
 import Loader from './../components/Loader'
-import { login } from './../action/userActions'
+import { register } from './../action/userActions'
 import { Link } from 'react-router-dom'
 import { Row, Col, Button, Form } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
 
-const LoginScreen = ({ location, history }) => {
+const RegisterScreen = ({ location, history }) => {
+  const [name, setname] = useState('')
   const [email, setemail] = useState('')
   const [password, setpassword] = useState('')
+  const [confirmpassword, setconfirmpassword] = useState('')
+  const [message, setmessage] = useState(null)
 
   const dispatch = useDispatch()
 
-  const userLogin = useSelector((state) => state.userLogin)
-  const { loading, error, userInfo } = userLogin
+  const userRegister = useSelector((state) => state.userRegister)
+  const { loading, error, userInfo } = userRegister
 
   const redirect = location.serach ? location.split('=')[1] : '/'
 
@@ -26,15 +29,29 @@ const LoginScreen = ({ location, history }) => {
 
   const submitHandler = (e) => {
     e.preventDefault()
-    dispatch(login(email, password))
+    if (password !== confirmpassword) {
+      setmessage('Passwords do not match')
+    } else dispatch(register(name, email, password))
   }
 
   return (
     <FormContainer>
-      <h1>Sign In</h1>
+      <h1>Sign Up</h1>
+      {message && <Message variant='danger'>{message}</Message>}
       {error && <Message variant='danger'>{error}</Message>}
       {loading && <Loader></Loader>}
       <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type='name'
+            placeholder='Enter Name'
+            value={name}
+            onChange={(e) => {
+              setname(e.target.value)
+            }}
+          ></Form.Control>
+        </Form.Group>
         <Form.Group controlId='email'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
@@ -57,15 +74,26 @@ const LoginScreen = ({ location, history }) => {
             }}
           ></Form.Control>
         </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Confirm Password'
+            value={confirmpassword}
+            onChange={(e) => {
+              setconfirmpassword(e.target.value)
+            }}
+          ></Form.Control>
+        </Form.Group>
         <Button type='submit' variant='primary'>
-          Sign In
+          Register
         </Button>
       </Form>
       <Row className='py-3'>
         <Col>
-          New Customer?{' '}
-          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-            Register
+          Have an Account?{' '}
+          <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+            Login
           </Link>
         </Col>
       </Row>
@@ -73,4 +101,4 @@ const LoginScreen = ({ location, history }) => {
   )
 }
 
-export default LoginScreen
+export default RegisterScreen
