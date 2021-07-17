4090a7a4b446c1d630463e232e7c841c71b54071
Utkarsh <ug123458@gmail.com>
2021-07-17 13:28:45 +0530

    added get order by id in backend

--- backend/controllers/Ordercontroller.js
+++ backend/controllers/Ordercontroller.js
@@ -31,9 +31,26 @@ const addOrderItems = asyncHandler(async (req, res) => {
       totalPrice,
     })
 
-    const createdOrder =await order.save()
+    const createdOrder = await order.save()
     res.status(201).json(createdOrder)
   }
 })
 
-export { addOrderItems }
+// @desc Get order by ID
+// @route GET /api/orders/:id
+// @access private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order Not Found')
+  }
+})
+
+export { addOrderItems, getOrderById }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { addOrderItems } from '../controllers/Ordercontroller.js'
+import { addOrderItems, getOrderById } from '../controllers/Ordercontroller.js'
 import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id').get(getOrderById)
 export default router
